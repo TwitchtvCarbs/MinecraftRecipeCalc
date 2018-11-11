cfef799ddbfe19c16d5cc2e3ac1ea689fda2002d
Carbs <Offthewall05641@gmail.com>
2018-11-11 03:47:45 -0700

    Cleaned up code in the database. Also broke some things lol

--- Database/Global_Recipe_Create_Database.gd
+++ Database/Global_Recipe_Create_Database.gd
@@ -3,69 +3,91 @@ extends Node
 var recipeToLoad = "acacia_planks"
 
 var url_database_recipe = "res://Recipes//1.13.2 JsonRecipes//"+String(recipeToLoad)+".json"
-var recipe_output = "null"
-var input_item = null
-var input_amount = 0
-var output_item = null
-var output_amount = 0
-var item_group = null
-var hasTag = false
+var recipe_output
+var input_item
+var input_amount 
+var output_item
+var output_amount
+var item_group
+var recipe_type
+var hasTag = false 
 var hasGroup = false
 
 
+
+
+
 func _ready():
-	print(url_database_recipe)
-	get_recipe()
-	if hasTag == true:
-		input_item = get_recipe()[1][0]["tag"]
-	else:
-		input_item = get_recipe()[1][0]["item"]
-		
-	output_item = get_recipe()[0][0]
-	
-	
-	if output_amount != 1:
-		output_amount = get_recipe()[0][1]
-	else:
-		output_amount = get_recipe()[0]
-	
-	var item_type = get_recipe()[2]
+	get_recipe()#run the function once to determine it the recipe has a tag and or group.
+
+	#TYPE
+	recipe_type = get_recipe()[0] #Shapless, Shaped or Smelting Recipe
+
+	#GROUP
 	if hasGroup == true:
-		item_group = get_recipe()[3]
+		item_group = get_recipe()[1]
 	else:
 		item_group = "None"
+
+	#INGREDIENTS / INPUT
+	if hasTag == true:#If the input item has a tag then use it if not use item.
+		input_item = get_recipe()[2][0]["tag"]
+	else:
+		input_item = get_recipe()[2][0]["item"]
+
+	#RESULT / OUTPUT
+	output_item = get_recipe()[3][0]
 	
-	recipe_output = "Input: "+String(input_amount)+" "+String(input_item) +"\n"+"Output: "+String(output_amount)+" "+String(output_item)+"\n"+String("Type: "+String(item_type)+"\n"+"Group: "+String(item_group))
+	#RESULT / OUTPUT AMOUNT
+	if output_amount != 1:#If the output amount is more then 1 then get how many.
+		output_amount = get_recipe()[3][1]
+	else:#Othewise get output and set the output amount to 1.
+		output_amount = get_recipe()[3]
+	
+	
+	#STRING OUTPUT
+	recipe_output = "Type: "+String(recipe_type)+"\n"+"Group: "+String(item_group)+"\n"+"Input: "+String(input_amount)+" "+String(input_item) +"\n"+"Output: "+String(output_amount)+" "+String(output_item)
 
 func get_recipe():
-	var itemData = {}
+	var itemData = {}#Create a dictionary for temp items recipe data
 	var recipe = []
 	itemData = Global_DataParser.load_data(url_database_recipe)
 	
+	#DOES RECIPE EXIST? If not then skip with a print message.
 	if !itemData.has("result"):
 		print ("This recipe does not exist!")
 		return
 		
-	if itemData["ingredients"][0].has("tag"):
-		hasTag = true
-		
-	recipe.append(itemData["result"].values())
-	recipe.append(itemData["ingredients"])
-	
-	
+	#TYPE
+	recipe.push_back(itemData["type"]) #0
+
+	#GROUP
+	if itemData.has("group"):
+		recipe.append(itemData["group"]) #1
+	else:
+		hasGroup = false
+
+	#INGREDIENTS / INPUT
+	recipe.push_back(itemData["ingredients"]) #2
+
+	#INGREDIENTS / INPUT AMOUNT
 	if itemData["ingredients"].size()<=1:
 		input_amount = 1
 	else:
 		input_amount = 0
-	recipe.append(itemData["type"])
+
+	#TAG
+	if itemData["ingredients"][0].has("tag"):
+		hasTag = true
 	
+	#RESULT / OUTPUT
+	recipe.push_back(itemData["result"].values()) #3
+
+	#RESULT / OUTPUT AMOUNT
 	if itemData["result"].values().size()==1:
 		output_amount = 1
 
-	if itemData.has("group"):
-		recipe.append(itemData["group"])
-	else:
-		hasGroup = false
+	#FUNCTION RETURN
 	return recipe
 	
 	
