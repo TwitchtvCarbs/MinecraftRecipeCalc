901d49ca762e231d8cb6a83ec991914fbf697d39
Carbs <Offthewall05641@gmail.com>
2018-11-17 20:08:33 -0700

    Removed unneed running of the get_recipe function sense we fixed the tag and group system. Also renamed get_recipe to get_recipe_shapelss

--- Database/Global_Recipe_Create_Database.gd
+++ Database/Global_Recipe_Create_Database.gd
@@ -1,6 +1,6 @@
 extends Node
 
-var recipeToLoad = "stone_button"
+var recipeToLoad = "acacia_planks"
 
 var url_database_recipe = "res://Recipes//1.13.2 JsonRecipes//"+String(recipeToLoad)+".json"
 var recipe_output
@@ -19,14 +19,13 @@ var arrayModifier = 0 #Used when an recipe doesn't have a group and sets to one 
 
 
 func _ready():
-	get_recipe()#run the function once to determine it the recipe has a tag and or group.
 
 	#TYPE
-	recipe_type = get_recipe()[0] #Shapless, Shaped or Smelting Recipe
+	recipe_type = get_recipe_shapeless()[0] #Shapless, Shaped or Smelting Recipe
 
 	#GROUP
 	if hasGroup == true:
-		item_group = get_recipe()[arrayModifier+1]
+		item_group = get_recipe_shapeless()[arrayModifier+1]
 	else:
 		arrayModifier = -1
 		item_group = "None"
@@ -34,16 +33,16 @@ func _ready():
 
 	#INGREDIENTS / INPUT
 	if hasTag == true:#If the input item has a tag then use it if not use item.
-		input_item = get_recipe()[arrayModifier+2][0]["tag"]
+		input_item = get_recipe_shapeless()[arrayModifier+2][0]["tag"]
 	else:
-		input_item = get_recipe()[arrayModifier+2][0]["item"]
+		input_item = get_recipe_shapeless()[arrayModifier+2][0]["item"]
 
 	#RESULT / OUTPUT
-	output_item = get_recipe()[arrayModifier+3][0]
+	output_item = get_recipe_shapeless()[arrayModifier+3][0]
 	
 	#RESULT / OUTPUT AMOUNT
 	if output_amount != 1:#If the output amount is more then 1 then get how many.
-		output_amount = get_recipe()[arrayModifier+3][1]
+		output_amount = get_recipe_shapeless()[arrayModifier+3][1]
 	else:#Othewise get output and set the output amount to 1.
 		output_amount = 1
 	
@@ -51,7 +50,7 @@ func _ready():
 	#STRING OUTPUT
 	recipe_output = "Type: "+String(recipe_type)+"\n"+"Group: "+String(item_group)+"\n"+"Input: "+String(input_amount)+" "+String(input_item) +"\n"+"Output: "+String(output_amount)+" "+String(output_item)
 
-func get_recipe():
+func get_recipe_shapeless():
 	var itemData = {}#Create a dictionary for temp items recipe data
 	var recipe = []
 	itemData = Global_DataParser.load_data(url_database_recipe)
