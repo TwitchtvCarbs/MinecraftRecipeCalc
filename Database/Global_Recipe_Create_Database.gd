5e0ad1e83fce71a54108ad6faea656580bc22a8b
Carbs <Offthewall05641@gmail.com>
2018-11-17 19:48:42 -0700

    Fixed up how groups are gathered and if a recipe does not have a group there is a modifier that subtracts from the array id accordingly

--- Database/Global_Recipe_Create_Database.gd
+++ Database/Global_Recipe_Create_Database.gd
@@ -1,6 +1,6 @@
 extends Node
 
-var recipeToLoad = "acacia_planks"
+var recipeToLoad = "stone_button"
 
 var url_database_recipe = "res://Recipes//1.13.2 JsonRecipes//"+String(recipeToLoad)+".json"
 var recipe_output
@@ -12,6 +12,7 @@ var item_group
 var recipe_type
 var hasTag = false 
 var hasGroup = false
+var arrayModifier = 0 #Used when an recipe doesn't have a group and sets to one less then recipes with a group.
 
 
 
@@ -25,26 +26,27 @@ func _ready():
 
 	#GROUP
 	if hasGroup == true:
-		item_group = get_recipe()[1]
+		item_group = get_recipe()[arrayModifier+1]
 	else:
+		arrayModifier = -1
 		item_group = "None"
 		
 	print(hasGroup)
 
 	#INGREDIENTS / INPUT
 	if hasTag == true:#If the input item has a tag then use it if not use item.
-		input_item = get_recipe()[2][0]["tag"]
+		input_item = get_recipe()[arrayModifier+2][0]["tag"]
 	else:
-		input_item = get_recipe()[2][0]["item"]
+		input_item = get_recipe()[arrayModifier+2][0]["item"]
 
 	#RESULT / OUTPUT
-	output_item = get_recipe()[3][0]
+	output_item = get_recipe()[arrayModifier+3][0]
 	
 	#RESULT / OUTPUT AMOUNT
 	if output_amount != 1:#If the output amount is more then 1 then get how many.
-		output_amount = get_recipe()[3][1]
+		output_amount = get_recipe()[arrayModifier+3][1]
 	else:#Othewise get output and set the output amount to 1.
-		output_amount = get_recipe()[3]
+		output_amount = get_recipe()[arrayModifier+3]
 	
 	
 	#STRING OUTPUT
@@ -82,6 +84,8 @@ func get_recipe():
 	#TAG
 	if itemData["ingredients"][0].has("tag"):
 		hasTag = true
+	else:
+		hasTag = false
 	
 	#RESULT / OUTPUT
 	recipe.push_back(itemData["result"].values()) #3
