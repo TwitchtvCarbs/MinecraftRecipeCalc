1c06a856ffe1b1a2f41a6d61e31b6df327bdda10
Carbs <Offthewall05641@gmail.com>
2018-12-11 00:21:16 -0700

    Recipe names while searching can now be typed in any case and/ or contain spaces making it easier to search with plain human readable inputs.

--- GUI/Main.gd
+++ GUI/Main.gd
@@ -1,13 +1,22 @@
 extends Node
 
+
 func _ready():
 	pass
 
 
 func _on_LineEdit_text_entered(new_text):
-	_getRecipeInfo(new_text.to_lower())
-	if !Global_Recipe_Create_Database.recipe_output == "This recipe does not exist!":
-		$VBox/OptionButton.add_item(new_text.to_lower())
+	var recentCount = $VBox/OptionButton.get_item_count()
+	var tempText = new_text.replace(" ","_")
+	_getRecipeInfo(tempText.to_lower())
+	if Global_Recipe_Create_Database.recipe_output != "This recipe does not exist!":
+		#if recentCount !=0:
+			#for i in recentCount:
+				#if $VBox/OptionButton.get_item_text(i) != new_text or $VBox/OptionButton.get_item_text(0) !=tempText:
+					#$VBox/OptionButton.add_item(tempText.to_lower())
+		#else:
+			#if $VBox/OptionButton.get_item_text(0) != new_text or $VBox/OptionButton.get_item_text(0) !=tempText:
+				$VBox/OptionButton.add_item(tempText.to_lower())
 	pass # replace with function body
 
 
